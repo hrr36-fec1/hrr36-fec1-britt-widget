ebb7183d3a2b20fd385cd31b168c1fa5817972c4
bzanetis <britt.zanetis19@gmail.com>
2019-02-21 21:29:10 -0600

    Switched out cors handling in server/index.js

--- server/index.js
+++ server/index.js
@@ -7,10 +7,25 @@ const {Details, db, save} = require('../database/index.js');
 let app = express();
 let port = 3002;
 
-app.all('/*', function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Headers", "X-Requested-With");
- next();
+// app.all('/*', function(req, res, next) {
+//  res.header("Access-Control-Allow-Origin", "*");
+//  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//  next();
+// });
+
+app.use(function(req, res, next) {
+  // allow CORS
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
 });
 
 app.use(bodyParser.json());
